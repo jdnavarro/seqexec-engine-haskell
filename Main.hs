b3e638c9e86182e994babe3b21fb4dacfc252153
Danny Navarro <j@dannynavarro.net>
2016-07-12 16:09:51 -0400

    Introduce user events

--- Main.hs
+++ Main.hs
@@ -1,8 +1,10 @@
 module Main where
 
-import Control.Concurrent (threadDelay)
+import Control.Concurrent (forkIO, threadDelay)
 import Control.Concurrent.Chan (Chan, newChan, readChan, writeChan)
+import Control.Monad (forever)
 import Data.Foldable (traverse_)
+import System.IO (BufferMode(NoBuffering), hSetBuffering, stdin)
 
 import Control.Concurrent.Async (concurrently)
 
@@ -16,7 +18,33 @@ newtype Observation = Observation (IO Result)
 
 data Result = Done | Error deriving (Show)
 
-data Event = Configured | Observed | Completed deriving (Show)
+data Event = EventSystem SystemEvent
+           | EventUser   UserEvent
+             deriving (Show)
+
+data SystemEvent = Configured
+                 | Observed
+                 | Completed
+                   deriving (Show)
+
+configured :: Event
+configured = EventSystem Configured
+
+observed :: Event
+observed = EventSystem Configured
+
+completed :: Event
+completed = EventSystem Completed
+
+data UserEvent = Paused
+               | Resumed
+                 deriving (Show)
+
+paused :: Event
+paused = EventUser Paused
+
+resumed :: Event
+resumed = EventUser Resumed
 
 execute :: Chan Event -> Sequence -> IO ()
 execute chan = traverse_ step
@@ -24,17 +52,18 @@ execute chan = traverse_ step
     step (Step (TcsConfig tcs) (InstConfig inst) (Observation obsv)) = do
         (tr, ir) <- concurrently tcs inst
         case (tr, ir) of
-             (Done, Done) -> writeChan chan Configured
+             (Done, Done) -> writeChan chan configured
              _            -> error "unhandled error in either TCS or Instrument"
         obr <- obsv
         case obr of
-             Done -> writeChan chan Observed
+             Done -> writeChan chan observed
              _    -> error "unhandled error in observation"
         -- Poison pill
-        writeChan chan Completed
+        writeChan chan completed
 
 data Status = Idle
             | Running
+            | Waiting
             | Failed
               deriving (Show)
 
@@ -43,12 +72,47 @@ handle chan = go Idle
   where
     go Idle = putStrLn "Output: Starting sequence" *> go Running
     go Failed = error "Unimplemented"
+    go Waiting = do
+      putStrLn "Output: Waiting for commands"
+      ev <- readChan chan
+      case ev of
+        EventSystem es ->
+          case es of
+            Configured -> do putStrLn "Output: TCS and Instrument configured, paused."
+                             go Waiting
+            Observed   -> do putStrLn "Output: Observation completed, paused"
+                             go Waiting
+            Completed  -> putStrLn "Output: Sequence completed"
+        EventUser ue ->
+          case ue of
+            Paused     -> do putStrLn "Output: Already paused"
+                             go Waiting
+            Resumed    -> do putStrLn "Output: Resuming..."
+                             go Running
     go Running = do
       ev <- readChan chan
       case ev of
-        Configured -> putStrLn "Output: TCS and Instrument configured" *> go Running
-        Observed   -> putStrLn "Output: Observation completed" *> go Running
-        Completed  -> putStrLn "Output: Sequence completed"
+        EventSystem es ->
+          case es of
+            Configured -> do putStrLn "Output: TCS and Instrument configured, continuing..."
+                             go Running
+            Observed   -> do putStrLn "Output: Observation completed, continuing..."
+                             go Running
+            Completed  -> putStrLn "Output: Sequence completed, continuing..."
+        EventUser ue ->
+          case ue of
+            Paused  -> do putStrLn "Output: Pausing..."
+                          go Waiting
+            Resumed -> do putStrLn "Output: Already resumed"
+                          go Running
+
+input :: Chan Event -> IO ()
+input chan = forever $ do
+  ic <- getChar
+  case ic of
+    'p' -> writeChan chan paused
+    'r' -> writeChan chan resumed
+    _   -> return ()
 
 sequence1 :: Sequence
 sequence1 = [
@@ -67,6 +131,8 @@ sequence1 = [
 
 main :: IO ()
 main = do
+  hSetBuffering stdin NoBuffering
   chan <- newChan
+  _ <- forkIO $ input chan
   _ <- concurrently (execute chan sequence1) (handle chan)
   putStrLn "Bye"
