c7c7d3ef1c2c08dfb12d4e73ccacdd41a8d4d954
Danny Navarro <j@dannynavarro.net>
2016-07-13 17:18:36 -0400

    WIP: Embed `Status` in an `MVar` accessible through a reader monad

--- Main.hs
+++ Main.hs
@@ -1,12 +1,16 @@
+{-# LANGUAGE LambdaCase #-}
 module Main where
 
 import Control.Concurrent (forkIO, threadDelay)
 import Control.Concurrent.Chan (Chan, newChan, readChan, writeChan)
+import Control.Concurrent.MVar
 import Control.Monad (forever)
 import Data.Foldable (traverse_)
 import System.IO (BufferMode(NoBuffering), hSetBuffering, stdin)
 
 import Control.Concurrent.Async (concurrently)
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.Reader
 
 type Sequence = [Step]
 
@@ -24,6 +28,7 @@ data Event = EventSystem SystemEvent
 
 data SystemEvent = Configured
                  | Observed
+                 | Paused
                  | Completed
                    deriving (Show)
 
@@ -33,106 +38,131 @@ configured = EventSystem Configured
 observed :: Event
 observed = EventSystem Configured
 
+paused :: Event
+paused = EventSystem Paused
+
 completed :: Event
 completed = EventSystem Completed
 
-data UserEvent = Paused
-               | Resumed
+data UserEvent = Pausing
+               | Resuming
                  deriving (Show)
 
-paused :: Event
-paused = EventUser Paused
-
-resumed :: Event
-resumed = EventUser Resumed
-
-execute :: Chan Event -> Sequence -> IO ()
-execute chan = traverse_ step
-  where
-    step (Step (TcsConfig tcs) (InstConfig inst) (Observation obsv)) = do
-        (tr, ir) <- concurrently tcs inst
-        case (tr, ir) of
-             (Done, Done) -> writeChan chan configured
-             _            -> error "unhandled error in either TCS or Instrument"
-        obr <- obsv
-        case obr of
-             Done -> writeChan chan observed
-             _    -> error "unhandled error in observation"
-        -- Poison pill
-        writeChan chan completed
-
-data Status = Idle
+data Status = Idling
             | Running
+            | Stopping
             | Waiting
-            | Failed
+            | Failing
+            | Finishing
               deriving (Show)
 
-handle :: Chan Event -> IO ()
-handle chan = go Idle
+type Telescope r = ReaderT (MVar Status) IO r
+
+switch :: Status -> Telescope ()
+switch st = ask >>= lift . flip modifyMVar_ (const $ pure st)
+
+status :: Telescope Status
+status = ask >>= lift . readMVar
+
+pausing :: Event
+pausing = EventUser Pausing
+
+resuming :: Event
+resuming = EventUser Resuming
+
+execute :: Chan Event -> Sequence -> Telescope ()
+execute chan = traverse_ step
   where
-    go Idle = putStrLn "Output: Starting sequence" *> go Running
-    go Failed = error "Unimplemented"
-    go Waiting = do
-      putStrLn "Output: Waiting for commands"
-      ev <- readChan chan
-      case ev of
-        EventSystem es ->
-          case es of
-            Configured -> do putStrLn "Output: TCS and Instrument configured, paused."
-                             go Waiting
-            Observed   -> do putStrLn "Output: Observation completed, paused"
-                             go Waiting
-            Completed  -> putStrLn "Output: Sequence completed"
-        EventUser ue ->
-          case ue of
-            Paused     -> do putStrLn "Output: Already paused"
-                             go Waiting
-            Resumed    -> do putStrLn "Output: Resuming..."
-                             go Running
-    go Running = do
-      ev <- readChan chan
-      case ev of
-        EventSystem es ->
-          case es of
-            Configured -> do putStrLn "Output: TCS and Instrument configured, continuing..."
-                             go Running
-            Observed   -> do putStrLn "Output: Observation completed, continuing..."
-                             go Running
-            Completed  -> putStrLn "Output: Sequence completed, continuing..."
-        EventUser ue ->
-          case ue of
-            Paused  -> do putStrLn "Output: Pausing..."
-                          go Waiting
-            Resumed -> do putStrLn "Output: Already resumed"
-                          go Running
+    step (Step (TcsConfig tcs) (InstConfig inst) (Observation obsv)) =
+        status >>= \case
+          Waiting -> error "Not implemented yet"
+          _ -> do
+            (tr, ir) <- lift $ concurrently tcs inst
+            case (tr, ir) of
+                (Done, Done) -> lift $ writeChan chan configured
+                _            -> error "unhandled error in either TCS or Instrument"
+            obr <- lift obsv
+            case obr of
+                Done -> lift $ writeChan chan observed
+                _    -> error "unhandled error in observation"
+            lift $ writeChan chan completed
+
+handle :: Chan Event -> Telescope ()
+handle chan = status >>= \case
+    Finishing -> lift $ putStrLn "Done"
+    st -> go st *> handle chan
+  where
+    go = \case
+      Idling  -> do
+        lift $ putStrLn "Output: Starting sequence"
+        switch Running
+      Failing -> error "Unimplemented"
+      Waiting -> do
+        lift $ putStrLn "Output: Waiting for commands"
+        lift (readChan chan) >>= \case
+          EventSystem es ->
+            case es of
+              Configured -> do
+                lift $ putStrLn "Output: TCS and Instrument configured, paused."
+                switch Waiting
+              Observed   -> do
+                lift $ putStrLn "Output: Observation completed, paused"
+                switch Waiting
+              Completed  -> lift $ putStrLn "Output: Step completed"
+          EventUser ue ->
+            case ue of
+              Pausing    -> do
+                lift $ putStrLn "Output: Already paused"
+                switch Waiting
+              Resuming   -> do
+                lift $ putStrLn "Output: Resuming..."
+                switch Running
+      Running ->
+        lift (readChan chan) >>= \case
+          EventSystem es ->
+            case es of
+              Configured -> do lift $ putStrLn "Output: TCS and Instrument configured, continuing..."
+                               switch Running
+              Observed   -> do lift $ putStrLn "Output: Observation completed, continuing..."
+                               switch Running
+              Completed  -> lift $ putStrLn "Output: Step completed, continuing..."
+          EventUser ue ->
+            case ue of
+              Pausing    -> do lift $ putStrLn "Output: Pausing..."
+                               switch Waiting
+              Resuming   -> do lift $ putStrLn "Output: Already resumed"
+                               switch Running
 
 input :: Chan Event -> IO ()
 input chan = forever $ do
   ic <- getChar
   case ic of
-    'p' -> writeChan chan paused
-    'r' -> writeChan chan resumed
+    'p' -> writeChan chan pausing
+    'r' -> writeChan chan resuming
     _   -> return ()
 
-sequence1 :: Sequence
-sequence1 = [
-    Step (TcsConfig   $ do putStrLn "System: Start TCS configuration for step 1"
+mkSequence :: Int -> Sequence
+mkSequence n = [
+    Step (TcsConfig   $ do putStrLn $ "System: Start TCS configuration for step " ++ show n
                            threadDelay 2000000
-                           putStrLn "System: Complete TCS configuration for step 1"
+                           putStrLn $ "System: Complete TCS configuration for step " ++ show n
                            return Done)
-         (InstConfig  $ do putStrLn "System: Start instrument configuration for step 1"
+         (InstConfig  $ do putStrLn $ "System: Start instrument configuration for step " ++ show n
                            threadDelay 2000000
-                           putStrLn "System: Complete instrument configuration for step 1"
+                           putStrLn $ "System: Complete instrument configuration for step " ++ show n
                            return Done)
-         (Observation $ do putStrLn "System: Start observation for step 1"
+         (Observation $ do putStrLn $ "System: Start observation for step " ++ show n
                            threadDelay 2000000
-                           putStrLn "System: Stop observation for step 1"
+                           putStrLn $ "System: Complete observation for step " ++ show n
                            return Done)]
 
 main :: IO ()
 main = do
   hSetBuffering stdin NoBuffering
   chan <- newChan
+  st0 <- newMVar Idling
   _ <- forkIO $ input chan
-  _ <- concurrently (execute chan sequence1) (handle chan)
+  let sequence0 = concatMap mkSequence [1..3]
+  _ <- concurrently (runReaderT (execute chan sequence0) st0)
+                    (runReaderT (handle chan) st0)
   putStrLn "Bye"
