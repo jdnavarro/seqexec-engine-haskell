7f9d93c5ee591acb78311f65c3ecf7b9eea7544f
Danny Navarro <j@dannynavarro.net>
2016-07-15 12:35:23 -0400

    Rewrite for event based concurrency
    This includes:
    - Actions on the systems spawn short-lived threads without carrying any state.
    - There is no executor, the `handler` is the only responsible for managing the
      state.
    - There is no need for `MVar` for synchronization.
    
    This supports pausing but not failure yet.

--- Main.hs
+++ Main.hs
@@ -1,168 +1,132 @@
 {-# LANGUAGE LambdaCase #-}
 module Main where
 
+import Prelude hiding (log)
 import Control.Concurrent (forkIO, threadDelay)
 import Control.Concurrent.Chan (Chan, newChan, readChan, writeChan)
-import Control.Concurrent.MVar
 import Control.Monad (forever)
-import Data.Foldable (traverse_)
+import Data.Bifunctor (second)
 import System.IO (BufferMode(NoBuffering), hSetBuffering, stdin)
 
-import Control.Concurrent.Async (concurrently)
-import Control.Monad.Trans.Class
-import Control.Monad.Trans.Reader
+import Control.Concurrent.Async (mapConcurrently)
+import Control.Monad.Trans.Class (lift)
+import Control.Monad.Trans.State (StateT, execStateT, get, gets, put, modify)
+import Control.Monad.Trans.Reader (ReaderT(runReaderT), ask)
 
-type Sequence = [Step]
+type Sequence = [[Action]]
 
-data Step = Step TcsConfig InstConfig Observation
+type Action = IO Result
 
-newtype TcsConfig   = TcsConfig   (IO Result)
-newtype InstConfig  = InstConfig  (IO Result)
-newtype Observation = Observation (IO Result)
+data Status = Running
+            | Waiting
+            deriving (Show)
+
+type SeqStatus = (Sequence, Status)
 
 data Result = Done | Error deriving (Show)
 
-data Event = EventSystem SystemEvent
-           | EventUser   UserEvent
-             deriving (Show)
+data Event = Pause
+           | Start
+           | Completed -- Action completed
+           | Failed    -- Action failed
+           | Synced    -- Parallel actions completed
+           | Finished
+           deriving (Show)
 
-data SystemEvent = Configured
-                 | Observed
-                 | Paused
-                 | Completed
-                   deriving (Show)
+configureTcs :: IO Result
+configureTcs = do
+    putStrLn "System: Start TCS configuration"
+    threadDelay 2000000
+    putStrLn "System: Complete TCS configuration"
+    return Done
 
-configured :: Event
-configured = EventSystem Configured
+configureInst :: IO Result
+configureInst = do
+    putStrLn "System: Start Instrument configuration"
+    threadDelay 2000000
+    putStrLn "System: Complete Instrument configuration"
+    return Done
 
-observed :: Event
-observed = EventSystem Configured
+observe :: IO Result
+observe = do
+    putStrLn "System: Start observation"
+    threadDelay 2000000
+    putStrLn "System: Complete observation"
+    return Done
 
-paused :: Event
-paused = EventSystem Paused
-
-completed :: Event
-completed = EventSystem Completed
-
-data UserEvent = Pausing
-               | Resuming
-                 deriving (Show)
-
-data Status = Idling
-            | Running
-            | Stopping
-            | Waiting
-            | Failing
-            | Finishing
-              deriving (Show)
-
-type Telescope r = ReaderT (MVar Status) IO r
-
-switch :: Status -> Telescope ()
-switch st = ask >>= lift . flip modifyMVar_ (const $ pure st)
-
-status :: Telescope Status
-status = ask >>= lift . readMVar
-
-pausing :: Event
-pausing = EventUser Pausing
-
-resuming :: Event
-resuming = EventUser Resuming
-
-execute :: Chan Event -> Sequence -> Telescope ()
-execute chan = traverse_ step
-  where
-    step (Step (TcsConfig tcs) (InstConfig inst) (Observation obsv)) =
-        status >>= \case
-          Waiting -> error "Not implemented yet"
-          _ -> do
-            (tr, ir) <- lift $ concurrently tcs inst
-            case (tr, ir) of
-                (Done, Done) -> lift $ writeChan chan configured
-                _            -> error "unhandled error in either TCS or Instrument"
-            obr <- lift obsv
-            case obr of
-                Done -> lift $ writeChan chan observed
-                _    -> error "unhandled error in observation"
-            lift $ writeChan chan completed
-
-handle :: Chan Event -> Telescope ()
-handle chan = status >>= \case
-    Finishing -> lift $ putStrLn "Done"
-    st -> go st *> handle chan
-  where
-    go = \case
-      Idling  -> do
-        lift $ putStrLn "Output: Starting sequence"
-        switch Running
-      Failing -> error "Unimplemented"
-      Waiting -> do
-        lift $ putStrLn "Output: Waiting for commands"
-        lift (readChan chan) >>= \case
-          EventSystem es ->
-            case es of
-              Configured -> do
-                lift $ putStrLn "Output: TCS and Instrument configured, paused."
-                switch Waiting
-              Observed   -> do
-                lift $ putStrLn "Output: Observation completed, paused"
-                switch Waiting
-              Completed  -> lift $ putStrLn "Output: Step completed"
-          EventUser ue ->
-            case ue of
-              Pausing    -> do
-                lift $ putStrLn "Output: Already paused"
-                switch Waiting
-              Resuming   -> do
-                lift $ putStrLn "Output: Resuming..."
-                switch Running
-      Running ->
-        lift (readChan chan) >>= \case
-          EventSystem es ->
-            case es of
-              Configured -> do lift $ putStrLn "Output: TCS and Instrument configured, continuing..."
-                               switch Running
-              Observed   -> do lift $ putStrLn "Output: Observation completed, continuing..."
-                               switch Running
-              Completed  -> lift $ putStrLn "Output: Step completed, continuing..."
-          EventUser ue ->
-            case ue of
-              Pausing    -> do lift $ putStrLn "Output: Pausing..."
-                               switch Waiting
-              Resuming   -> do lift $ putStrLn "Output: Already resumed"
-                               switch Running
-
-input :: Chan Event -> IO ()
-input chan = forever $ do
-  ic <- getChar
-  case ic of
-    'p' -> writeChan chan pausing
-    'r' -> writeChan chan resuming
+input :: Chan Event -> IO r
+input chan = forever $ getChar >>= \case
+    'p' -> writeChan chan Pause
+    's' -> writeChan chan Start
     _   -> return ()
 
-mkSequence :: Int -> Sequence
-mkSequence n = [
-    Step (TcsConfig   $ do putStrLn $ "System: Start TCS configuration for step " ++ show n
-                           threadDelay 2000000
-                           putStrLn $ "System: Complete TCS configuration for step " ++ show n
-                           return Done)
-         (InstConfig  $ do putStrLn $ "System: Start instrument configuration for step " ++ show n
-                           threadDelay 2000000
-                           putStrLn $ "System: Complete instrument configuration for step " ++ show n
-                           return Done)
-         (Observation $ do putStrLn $ "System: Start observation for step " ++ show n
-                           threadDelay 2000000
-                           putStrLn $ "System: Complete observation for step " ++ show n
-                           return Done)]
+-- * Executor
+
+type Telescope r = StateT SeqStatus (ReaderT (Chan Event) IO) r
+
+handler :: Telescope r
+handler = forever $ receive >>= \case
+    Start -> log "Output: Starting" *> switch Running *> execute
+    Pause -> log "Output: Paused" *> switch Waiting
+    Completed -> log "Output: Action completed"
+    Failed -> log "Output: Action failed"
+    Synced -> log "Output: Parallel actions completed" *> execute
+    Finished -> log "Output: Finished" *> switch Waiting
+
+log :: String -> Telescope ()
+log = lift . lift . putStrLn
+
+receive :: Telescope Event
+receive = lift ask
+      >>= lift . lift . readChan
+
+send :: Event -> Telescope ()
+send ev = lift ask
+      >>= lift . lift . flip writeChan ev
+
+execute :: Telescope ()
+execute = status >>= \case
+    Running -> go
+    Waiting -> return ()
+  where
+    go :: Telescope ()
+    go = do
+        actions <- pop
+        chan <- lift ask
+        _ <- lift . lift $ mapConcurrently (exe chan) actions
+        send Synced
+      where
+        exe chan action = do
+            r <- action
+            case r of
+              Done  -> writeChan chan Completed
+              Error -> writeChan chan Failed
+
+pop :: Telescope [Action]
+pop = do
+    (seq', st) <- get
+    case seq' of
+         (x:xs) -> put (xs, st) *> return x
+         [] -> send Finished *> return []
+
+switch :: Status -> Telescope ()
+switch = modify . second . const
+
+status :: Telescope Status
+status = gets snd
+
+sequence0 :: Sequence
+sequence0 = [
+    [ configureTcs, configureInst ]
+  , [ observe ]
+  , [ configureTcs, configureInst ]
+  , [ observe ]
+  ]
 
 main :: IO ()
 main = do
-  hSetBuffering stdin NoBuffering
-  chan <- newChan
-  st0 <- newMVar Idling
-  _ <- forkIO $ input chan
-  let sequence0 = concatMap mkSequence [1..3]
-  _ <- concurrently (runReaderT (execute chan sequence0) st0)
-                    (runReaderT (handle chan) st0)
-  putStrLn "Bye"
+    hSetBuffering stdin NoBuffering
+    chan <- newChan
+    _ <- forkIO $ input chan
+    _ <- flip runReaderT chan . flip execStateT (sequence0, Waiting) $ handler
+    putStrLn "Done"
